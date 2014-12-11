faacdaaf73f7d948fe68f5b0a944cbef8f06f367
Jonathan Webb <jonathan.webb@acquia.com>
2014-12-11 08:11:08 -0500

    experimental

---
+++ manifests/experimental.pp
@@ -0,0 +1,8 @@
+# Experiemental preferences for webbj74
+# These are items which currently don't have passing tests
+class setup_webbj74::experimental {
+  include setup_webbj74::php
+  include setup_webbj74::brews
+  include setup_webbj74::projects::d8li
+}
+
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,9 @@
 # Preferences for webbj74
+# See also experimental.pp
 class setup_webbj74 {
   include setup_webbj74::apps
   include setup_webbj74::fonts
   include setup_webbj74::gitconfig
   include setup_webbj74::osx
-  # Test failures
-  # include setup_webbj74::php
-  # include setup_webbj74::brews
-  # include setup_webbj74::projects::d8li
-
 }
 
